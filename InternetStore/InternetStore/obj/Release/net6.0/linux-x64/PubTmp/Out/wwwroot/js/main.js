a3a2b7b326e1ec9da8610665a6b50152fe828221
Vitalii-tmp <vitaliilchuk6@gmail.com>
2024-02-17 02:42:25 +0200

    release bild

---
+++ InternetStore/InternetStore/obj/Release/net6.0/linux-x64/PubTmp/Out/wwwroot/js/main.js
@@ -0,0 +1,5 @@
+﻿let navIconsContainer = document.getElementById('nav-icons');
+let navIcons = document.querySelector('.header-icons');
+navIconsContainer.appendChild(navIcons);
+navIcons.classList.remove('d-none');
+
